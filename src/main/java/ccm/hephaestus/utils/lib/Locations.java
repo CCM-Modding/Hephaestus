a66e3db7ce6069d579ec9765987db69ea43186b3
Captain Shadows <captain_shadows@aol.com>
2013-08-14 23:55:00 -0400

    Update Locations.java

--- src/main/java/ccm/hephaestus/utils/lib/Locations.java
+++ src/main/java/ccm/hephaestus/utils/lib/Locations.java
@@ -6,8 +6,6 @@ public final class Locations {
 
     public static final String TEXTURE = MOD_ID + ":";
 
-    public static final String LANGUAGE_FILE = "/assets/" + MOD_ID + "/lang/";
-
     public static final String PROXY = "ccm." + MOD_ID + ".core.proxy.";
 
     public static final String CLIENT_PROXY = PROXY + "ClientProxy";
